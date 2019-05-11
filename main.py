573c9754130659ce5cacfcf80fb0b8d81e6514dc
Jess Henry <jessjdhenry@gmail.com>
2019-05-11 16:16:17 -0500

    Added signup/login templates and routes

--- main.py
+++ main.py
@@ -12,10 +12,23 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = (db.String(60))
+    password = (db.String(30))
+    blogs = db.relationship('Blog', backref='owner')
+
+def __init_(self, username, password):
+    self.username = username
+    self.password = password
 
 @app.route('/')
 def index():
@@ -37,6 +50,7 @@ def listings():
 def new_post():
     title = ''
     body = ''
+    owner = ''
     title_error = ''
     body_error = ''
 
@@ -61,6 +75,14 @@ def new_post():
 
     if request.method == 'GET':
          return render_template("form.html", web_title="Create a New Post")
+
+@app.route('/signup', methods= ['get'])
+def signup():
+    return render_template('signup.html', web_title='Create an Account!')
+
+@app.route('/login', methods=['get'])
+def login():
+    return render_template('login.html', web_title='Go on and Log in!')
 
 if __name__ == '__main__':
     app.run()
